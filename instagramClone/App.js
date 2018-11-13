ce3fa60e1163454e35a323fd7af01168cbb6d861
Jose Diestra Melendez <jozhetak@bitbucket.org>
2018-11-13 15:02:53 -0500

    Complete Code

---
+++ instagramClone/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import InstagramClone from './src/InstagramClone.js'
+
+export default class App extends Component{
+  render() {
+    return (
+        <InstagramClone />
+    );
+  }
+}
